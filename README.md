cf1b5eef355bf37f17ef87248e249f283897461b
phxtho <phathopukwana@gmail.com>
2020-07-08 00:47:58 +0200

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# clojure-brave-and-true
+
+Code exercises while I'm reading [Clojure for the Brave and True](https://www.braveclojure.com/)
+
+## License
+
+Copyright © 2020 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clojure-brave-and-true "0.1.0-SNAPSHOT"
+  :description "Code exercises while I'm reading Clojure for the Brave and True"
+  :url "https://www.braveclojure.com/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
